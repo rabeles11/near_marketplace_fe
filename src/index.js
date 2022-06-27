8d78bcfbe5f0a5c3b14f1b587e679c5a1a37c2f1
Rabeles <9david@seznam.cz>
2022-06-27 17:40:52 +0200

    Final project with error

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { initializeContract } from "./utils/near";
 
+import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "!style-loader!css-loader!react-toastify/dist/ReactToastify.css"
+import { injectStyle } from "react-toastify/dist/inject-style";
+injectStyle();
 window.nearInitPromise = initializeContract()
   .then(() => {
     ReactDOM.render(
       <React.StrictMode>
         <App />
+        
       </React.StrictMode>,
       document.getElementById("root")
     );
