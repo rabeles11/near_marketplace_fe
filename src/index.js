ca1f95bc38a4eb38fb03de36e483d83101089e45
Rabeles <9david@seznam.cz>
2022-06-27 17:41:28 +0200

    Final project with error

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { initializeContract } from "./utils/near";
 import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "!style-loader!css-loader!react-toastify/dist/ReactToastify.css"
-import { injectStyle } from "react-toastify/dist/inject-style";
-injectStyle();
+
 window.nearInitPromise = initializeContract()
   .then(() => {
     ReactDOM.render(
